a139833a821c6d7cc9b455b757f0ae2ac321c9c9
lakshay148 <lakshay148@gmail.com>
2020-01-20 11:25:56 +0530

    product metrics api changes

--- src/main/java/com/quizcore/quizapp/controller/ProductController.java
+++ src/main/java/com/quizcore/quizapp/controller/ProductController.java
@@ -14,15 +14,13 @@ import com.quizcore.quizapp.service.MediaStorageService;
 import com.quizcore.quizapp.service.OnboardingService;
 import com.quizcore.quizapp.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.websocket.server.PathParam;
 import java.util.ArrayList;
 import java.util.UUID;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("${base.endpoint}/api/v1/product")
 public class ProductController {
@@ -89,7 +87,7 @@ public class ProductController {
         return response;
     }
 
-    @GetMapping("/metrics/{productId}")
+    @GetMapping("/{productId}/metrics")
     public BaseResponse<ProductMetrics> getProductMetrics(@PathVariable("productId") String productId){
         ProductMetrics productMetrics = productService.getMetrics(UUID.fromString(productId));
         SuccessResponse<ProductMetrics> response = new SuccessResponse<ProductMetrics>("Product Metrics");
